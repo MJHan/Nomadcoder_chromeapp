2a292109e3be1238d020d22229e70c73f99919a9
MJHan <hamatoto@gmail.com>
2023-07-21 21:17:34 +0900

    chrome app 만드는중

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,5 @@
-const weather = document.querySelector("#weather span:first-child");
-const city = document.querySelector("#weather span:last-child");
+const weather = document.querySelector("#weather div:first-child");
+const city = document.querySelector("#weather div:last-child");
 const API_KEY = "16404a0f651db6245890f34e76fc0e29";
 
 function onGeoOk(position) {
@@ -8,7 +8,7 @@ function onGeoOk(position) {
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url).then((response) =>
     response.json().then((data) => {
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`;
+      weather.innerText = `${data.weather[0].main} ${data.main.temp}℃`;
       city.innerText = data.name;
       console.log(data);
     })
